3fbcf6c3795ff20b6965b74237067faf1b23ef16
Andrés Contreras Guillén <me@ancongui.com>
2025-11-02 00:35:03 +0100

    feat: add MapStruct entity-DTO mappers
    - Add SupplyChainFinanceAgreementMapper for agreement conversions
    - Add CounterpartyMapper for counterparty transformations
    - Add SupplyChainFinanceInvoiceMapper for invoice mapping
    - Add SupplyChainFinanceAdvanceMapper for advance data mapping
    - Add SupplyChainFinanceFeeMapper for fee structure mapping
    - Add SupplyChainFinanceSettlementMapper for settlement mapping
    - Mappers handle bidirectional entity-DTO conversions

---
+++ core-lending-supply-chain-finance-core/src/main/java/com/firefly/core/lending/supplychainfinance/core/mappers/SupplyChainFinanceInvoiceMapper.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2025 Firefly Software Solutions Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.firefly.core.lending.supplychainfinance.core.mappers;
+
+import com.firefly.core.lending.supplychainfinance.interfaces.dtos.SupplyChainFinanceInvoiceDTO;
+import com.firefly.core.lending.supplychainfinance.models.entities.SupplyChainFinanceInvoice;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SupplyChainFinanceInvoiceMapper {
+    SupplyChainFinanceInvoiceDTO toDTO(SupplyChainFinanceInvoice entity);
+    SupplyChainFinanceInvoice toEntity(SupplyChainFinanceInvoiceDTO dto);
+}
